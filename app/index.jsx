0fad5fcdfe9cc4bb27face7e95494e76b33f8489
suffy <suffy.yanuar@gmail.com>
2024-09-15 19:49:28 +0700

    setup awal

---
+++ app/index.jsx
@@ -0,0 +1,116 @@
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  StatusBar,
+  Pressable,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import { wp, hp } from "./../helpers/common";
+import Animated, { FadeInDown } from "react-native-reanimated";
+import { LinearGradient } from "expo-linear-gradient";
+import { theme } from "./../constants/theme";
+import { useRouter } from "expo-router";
+
+const WelcomeScreen = () => {
+  const router = useRouter();
+  return (
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <Image
+        source={require("./../assets/images/welcome.png")}
+        style={styles.bgImage}
+        resizeMode="cover"
+      />
+      {/* linear gradient */}
+      <Animated.View entering={FadeInDown.duration(600)} style={{ flex: 1 }}>
+        <LinearGradient
+          colors={[
+            "rgba(255,255,255,0)",
+            "rgba(255,255,255,0.5)",
+            "white",
+            "white",
+          ]}
+          style={styles.gradient}
+          start={{ x: 0.5, y: 0 }}
+          end={{ x: 0.5, y: 0.8 }}
+        />
+        {/* content */}
+        <View style={styles.contentContaier}>
+          <Animated.Text
+            entering={FadeInDown.delay(400).springify()}
+            style={styles.title}
+          >
+            Jala Hebron
+          </Animated.Text>
+          <Animated.Text
+            entering={FadeInDown.delay(500).springify()}
+            style={styles.punchline}
+          >
+            version 1.0.0
+          </Animated.Text>
+          <Animated.View
+            entering={FadeInDown.delay(600).springify()}
+            style={styles.startButton}
+          >
+            <TouchableOpacity onPress={() => router.push("/home")}>
+              <Text style={styles.startText}>Start Explore</Text>
+            </TouchableOpacity>
+          </Animated.View>
+        </View>
+      </Animated.View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  bgImage: {
+    height: hp(100),
+    width: wp(100),
+    position: "absolute",
+  },
+  container: {
+    flex: 1,
+  },
+  gradient: {
+    width: wp(100),
+    height: hp(65),
+    bottom: 0,
+    position: "absolute",
+  },
+  contentContaier: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    gap: 14,
+  },
+  title: {
+    fontSize: hp(7),
+    color: theme.colors.neutral(0.9),
+    fontWeight: theme.fontWeights.bold,
+  },
+  punchline: {
+    fontSize: hp(2),
+    letterSpacing: 1,
+    marginBottom: 10,
+    fontWeight: theme.fontWeights.medium,
+  },
+  startButton: {
+    marginBottom: 50,
+    backgroundColor: theme.colors.neutral(0.9),
+    padding: 15,
+    paddingHorizontal: 90,
+    borderRadius: theme.radius.xl,
+    borderCurve: "continuous",
+  },
+  startText: {
+    color: theme.colors.white,
+    fontSize: hp(3),
+    fontWeight: theme.fontWeights.medium,
+    letterSpacing: 1,
+  },
+});
+
+export default WelcomeScreen;
---
+++ components/Categories.jsx
@@ -0,0 +1,70 @@
+import { View, Text, StyleSheet, FlatList, Pressable } from "react-native";
+import React from "react";
+import { data } from "../constants/data";
+import { wp, hp } from "../helpers/common";
+import { theme } from "../constants/theme";
+import Animated, { FadeInRight } from "react-native-reanimated";
+
+const Categories = ({ activeCategory, handleChangeCategory }) => {
+  return (
+    <FlatList
+      horizontal
+      contextContainerStyle={styles.flatListContainer}
+      showsHorizontalScrollIndicator={false}
+      data={data.categories}
+      keyExtractor={(item) => item}
+      renderItem={({ item, index }) => (
+        <CategoryItem
+          isActive={activeCategory == item}
+          handleChangeCategory={handleChangeCategory}
+          title={item}
+          key={index}
+        />
+      )}
+    />
+  );
+};
+
+const CategoryItem = ({ title, index, isActive, handleChangeCategory }) => {
+  let color = isActive ? theme.colors.white : theme.colors.neutral(0.8);
+  let backgroundColor = isActive
+    ? theme.colors.neutral(0.8)
+    : theme.colors.white;
+
+  return (
+    <Animated.View
+      entering={FadeInRight.delay(index * 200)
+        .duration(1000)
+        .springify()
+        .damping(14)}
+    >
+      <Pressable
+        onPress={() => handleChangeCategory(isActive ? null : title)}
+        style={[styles.category, { backgroundColor }]}
+      >
+        <Text style={[styles.title, { color }]}>{title}</Text>
+      </Pressable>
+    </Animated.View>
+  );
+};
+
+const styles = StyleSheet.create({
+  flatListContainer: {
+    paddingHorizontal: wp(4),
+    gap: 8,
+  },
+  category: {
+    padding: 12,
+    paddingHorizontal: 15,
+    borderWidth: 1,
+    borderColor: theme.colors.grayBG,
+    borderRadius: theme.radius.lg,
+    borderCurve: "continuous",
+  },
+  title: {
+    fontSize: hp(1.8),
+    fontWeight: theme.fontWeights.medium,
+  },
+});
+
+export default Categories;
